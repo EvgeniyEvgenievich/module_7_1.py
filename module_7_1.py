2b2003c54c91ac3847b20e27b6d6314d89c3a367
EvgeniyEvgenievich <Dedjoze@gmail.com>
2024-11-25 11:37:56 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -1,3 +1,6 @@
+from pprint import pprint
+
+
 class Product:
     def __init__(self, name: str, weight: float, category: str):
         self.name = name
@@ -13,19 +16,21 @@ class Shop:
         self.__file_name = 'products.txt'
 
     def get_products(self):
+        self.__file_name = 'products.txt'
         file = open(self.__file_name, 'r')
         products = file.read()
         file.close()
+
         return products
 
     def add(self, *products):
-        for i in products:
-            if (i.name in self.get_products()) and (str(i.weight) in self.get_products()):
-                print(f'Продукт {i.name} уже есть в магазине')
-            else:
+        for product in products:
+            if product.name not in self.get_products():
                 file = open(self.__file_name, 'a')
-                file.write(f'{i}\n')
-                file.close()
+                file.write(product.__str__() + '\n')
+
+            else:
+                print(f'Продукт {product.name} уже есть в магазине')
 
 
 s1 = Shop()
@@ -33,11 +38,7 @@ p1 = Product('Potato',  50.5, 'Vegitables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 s1.add(p1, p2, p3)
-
-print(p2) # __str__
-
 s1.get_products()
-
-s1.add(p1, p2, p3)
-
-print(s1.get_products())
+print(p1)
+print(p2)
+print(p3)
